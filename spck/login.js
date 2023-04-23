8282f4cc3ab09fe3f2e58faf2d0d3fe1914d8fe8
nhat1303 <nhathahaha13@gmail.com>
2023-04-23 21:19:36 +0700

    spck

--- spck/login.js
+++ spck/login.js
@@ -1,20 +1,4 @@
-const productContainers = [...document.querySelectorAll('.product-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
-
-productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
-
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
-
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
-
+// MENU 
 
 let account_avatar = document.getElementById("avatar-account")
 let login = document.getElementById("login")
