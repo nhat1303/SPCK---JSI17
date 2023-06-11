c68d4f72da327699e941ea344a4672f0e762ff6c
nhat1303 <nhathahaha13@gmail.com>
2023-06-11 21:34:19 +0700

    update

---
+++ spck/cart.js
@@ -0,0 +1,3 @@
+document.getElementById("avatar-account").addEventListener('click', function(){
+    window.location.href = "index8.html";
+})
---
+++ spck/test.js
@@ -0,0 +1,280 @@
+// let imgDiv = document.querySelector('.avatar-box')
+let file = document.querySelector('#file')
+let img = document.querySelector('#img-box')
+let uploadBtn = document.querySelector('#uploadAvatar')
+
+file.addEventListener("change", function(){
+    const choosedFile = this.files[0]
+
+    if ( choosedFile){
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function(){
+            img.setAttribute('src', reader.result)
+        })
+
+        reader.readAsDataURL(choosedFile)
+    }
+})
+
+
+window.addEventListener("load", onLoad, false);
+                
+function onLoad(){
+    Draggable(document.getElementById('img-box'));
+    Resizeable(document.getElementById('img-box'));
+}
+
+function Draggable(box){
+    var origX, origY, newX, newY = 0;
+    box.addEventListener("mousedown", (event)=>{
+        event = event || window.event;
+        event.preventDefault();
+        origX = event.clientX;
+        origY = event.clientY;
+        document.onmousemove = boxMove;
+        document.onmouseup = stopBoxMove;
+    });
+    function boxMove(event){
+        event = event || window.event;
+        event.preventDefault();
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        box.style.top = (box.offsetTop + newY) + "px";
+        box.style.left = (box.offsetLeft + newX) + "px";
+    }
+    function stopBoxMove(){
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+function Resizeable(box){
+    box.addEventListener("mousemove", mousemove);
+    var boxStyle = window.getComputedStyle(box);
+    var origX, origY, newX, newY = 0;
+    function mousemove(event){
+        if (event.offsetX <= 0 && event.offsetY <= 0){
+            box.style.cursor = "nwse-resize";
+            document.onmousedown = resize;
+        }
+        else if(event.offsetX >= parseInt(boxStyle.width)
+        && event.offsetY <=0){
+            box.style.cursor = "nesw-resize";
+            document.onmousedown = resize;
+        }
+        else if(event.offsetX <= 0 && event.offsetY >= parseInt(boxStyle.height)){
+            box.style.cursor = "nesw-resize";
+            document.onmousedown = resize;
+        }
+        else if(event.offsetX >= parseInt(boxStyle.width)
+        && event.offsetY >= parseInt(boxStyle.height)){
+            box.style.cursor = "nwse-resize";
+            document.onmousedown = resize;
+        }
+        else if(event.offsetX <= 0){
+            box.style.cursor = "ew-resize";
+            document.onmousedown = resize;
+        }
+        else if(event.offsetX >= parseInt(boxStyle.width)){
+            box.style.cursor ="ew-resize";
+            document.onmousedown = resize;
+        }
+        else if(event.offsetY <= 0){
+            box.style.cursor ="ns-resize";
+            document.onmousedown = resize;
+        }
+        else if(event.offsetY >= parseInt(boxStyle.height)){
+            box.style.cursor ="ns-resize";
+            document.onmousedown = resize;
+        }
+        else if(!document.onmousemove){
+            box.style.cursor = "move";
+            document.onmousedown = null;
+        }
+    }
+    function resize(event){
+        event = event || window.event;
+        event.preventDefault();
+        origX = event.clientX;
+        origY = event.clientY;
+        if (event.offsetX <= 0 && event.offsetY <= 0){
+            document.onmousemove = resizeTl;
+        }
+        else if(event.offsetX >= parseInt(boxStyle.width)
+        && event.offsetY <=0){
+            document.onmousemove = resizeTr;
+        }
+        else if(event.offsetX <= 0 && event.offsetY >= parseInt(boxStyle.height)){
+            document.onmousemove = resizeBl;
+        }
+        else if(event.offsetX >= parseInt(boxStyle.width)
+        && event.offsetY >= parseInt(boxStyle.height)){
+            document.onmousemove = resizeBr;
+        }
+        else if(event.offsetX <= 0){
+            document.onmousemove = resizeLeft;
+        }
+        else if(event.offsetY <= 0){
+            document.onmousemove = resizeTop;
+        }
+        else if(event.offsetX >= parseInt(boxStyle.width)){
+            document.onmousemove = resizeRight;
+        }
+        else if(event.offsetY >= parseInt(boxStyle.height)){
+            document.onmousemove = resizeBottom;
+        }
+        document.onmouseup = stopResize;
+    }
+    function resizeTl(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        topRS();
+        leftRS();        
+    }
+    function resizeTr(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        topRS();
+        rightRS();
+    }
+    function resizeBl(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        bottomRS();
+        leftRS();
+    }
+    function resizeBr(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        bottomRS();
+        rightRS();
+    }
+    function resizeLeft(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newX = event.clientX - origX;
+        origX = event.clientX;
+        leftRS();
+
+    }
+    function resizeTop(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newY = event.clientY - origY;
+        origY = event.clientY;
+        topRS()
+    }
+    function resizeRight(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newX = event.clientX - origX;
+        origX = event.clientX;
+        rightRS();
+
+    }
+    function resizeBottom(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newY = event.clientY - origY;
+        origY = event.clientY;
+        bottomRS();
+    }
+    function stopResize(event){
+        document.onmousemove = null;
+        document.onmouseup = null;
+        document.documentElement.style.cursor = "";
+    }
+
+    function leftRS(){
+        //edge case, if box is at minimum specs, we want a certain behaviour
+        if((parseInt(boxStyle.minWidth) == parseInt(boxStyle.width))){
+            if(newX <= 0 && event.clientX < parseInt(boxStyle.left)){
+                box.style.left = parseInt(boxStyle.left) + newX + "px";
+                box.style.width = (parseInt(boxStyle.width) - newX) + "px"
+            }
+        }
+        else{
+            console.log(parseInt(boxStyle.width) + " " + newX + " " + boxStyle.minWidth)
+            if((parseInt(boxStyle.width) - newX) < parseInt(boxStyle.minWidth)){
+                box.style.left = (parseInt(boxStyle.left) + (parseInt(boxStyle.width) - parseInt(boxStyle.minWidth))) + "px";
+                box.style.width = parseInt(boxStyle.minWidth) + "px"            
+            }
+            else{
+                box.style.left = parseInt(boxStyle.left) + newX + "px";
+                box.style.width = (parseInt(boxStyle.width) - newX) + "px"
+            }
+        }
+    }
+    function topRS(){
+        //edge case, if box is at minimum specs, we want a certain behaviour
+        if((parseInt(boxStyle.minHeight) == parseInt(boxStyle.height))){
+            prevTop = parseInt(boxStyle.top);
+            if(newY <= 0 && event.clientY < parseInt(boxStyle.top)){
+                box.style.top = parseInt(boxStyle.top) + newY + "px";
+                box.style.height = (parseInt(boxStyle.height) - newY) + "px"
+            }
+        }
+        else{
+            if((parseInt(boxStyle.height) - newY) < parseInt(boxStyle.minHeight)){
+                box.style.top = (parseInt(boxStyle.top) + (parseInt(boxStyle.height) - parseInt(boxStyle.minHeight))) + "px";
+                box.style.height = parseInt(boxStyle.minHeight) + "px"            
+            }
+            else{
+                box.style.top = parseInt(boxStyle.top) + newY + "px";
+                box.style.height = (parseInt(boxStyle.height) - newY) + "px"
+            }
+        }
+    }
+    function rightRS(){
+        //edge case, if box is at minimum specs, we want a certain behaviour
+        if((parseInt(boxStyle.minWidth) == parseInt(boxStyle.width))){
+            if(newX >= 0 && event.clientX > parseInt(boxStyle.left) 
+            + parseInt(boxStyle.width)){
+                box.style.width = (parseInt(boxStyle.width) + newX) + "px"
+            }
+        }
+        else{
+            box.style.width = (parseInt(boxStyle.width) + newX) + "px"
+        }
+    }
+    function bottomRS(){
+        //edge case, if box is at minimum specs, we want a certain behaviour
+        if((parseInt(boxStyle.minHeight) == parseInt(boxStyle.height))){
+            prevTop = parseInt(boxStyle.top);
+            if(newY >= 0 && event.clientY > parseInt(boxStyle.top) +
+            parseInt(boxStyle.height)){
+                box.style.height = 
+                (parseInt(window.getComputedStyle(box).height) + newY) + "px";
+            }
+        }
+        else{
+            box.style.height = (parseInt(boxStyle.height) + newY) + "px";
+        }
+    }
+}
